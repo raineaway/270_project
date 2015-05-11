75ad82762839106bee4624c87f126fed372214a9
Cai Abbass <cai.abbass@gmail.com>
2015-05-11 20:30:48 +0000

    schema test 2

--- schema.sql
+++ schema.sql
@@ -1,2 +1,25 @@
-create database cheduler;
+create database Scheduler;
+
+--create tables
+
+create table User(
+    uID int NOT NULL AUTO_INCREMENT,
+    lastName varchar(255),
+    firstName varchar(255),
+    username varchar(255),
+    emailAddress varchar(255),
+    PRIMARY KEY (uID)
+);
+
+create table Calendar(
+    calID int NOT NULL AUTO_INCREMENT,
+    calName
+    calColor
+    PRIMARY KEY (uID),
+    FOREIGN KEY (calID) REFERENCES User(uID)
+);
+
+create table Event(
+    
+);
 
