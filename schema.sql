1ec2f2c9b2b005e59b0d7304a6e9c6c21c6925e7
Cai <cai.abbass@gmail.com>
2015-05-13 01:24:00 +0800

    added schema.sql and testdata.sql

--- schema.sql
+++ schema.sql
@@ -1,25 +1,30 @@
-create database Scheduler;
+CREATE DATABASE Scheduler;
 
 --create tables
 
-create table User(
-    uID int NOT NULL AUTO_INCREMENT,
+CREATE TABLE User(
+    uID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     lastName varchar(255),
     firstName varchar(255),
-    username varchar(255),
-    emailAddress varchar(255),
-    PRIMARY KEY (uID)
+    username varchar(255) NOT NULL,
+    emailAddress varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
 );
 
-create table Calendar(
-    calID int NOT NULL AUTO_INCREMENT,
-    calName
-    calColor
-    PRIMARY KEY (uID),
-    FOREIGN KEY (calID) REFERENCES User(uID)
+CREATE TABLE Calendar(
+    calenID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    calenName varchar(255) NOT NULL,
+    calenColor varchar(255),
+    uID int NOT NULL,
+    FOREIGN KEY (uID) REFERENCES User(uID)
 );
 
-create table Event(
-    
+CREATE TABLE Event(
+    eventID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    eventName varchar(255) NOT NULL,
+    allDay BOOLEAN NOT NULL DEFAULT 0,
+    dateStart DATETIME DEFAULT CURRENT_TIMESTAMP,
+    dateEnd DATETIME DEFAULT CURRENT_TIMESTAMP,
+    calenID int NOT NULL,
+    FOREIGN KEY (calenID) REFERENCES Calendar(calenID)
 );
-
