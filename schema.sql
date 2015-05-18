64557c84df3f30ab2f99b4de5dc25b3139a50353
DRaine Edrosa <ltedrosa@gmail.com>
2015-05-18 14:08:48 +0800

    modified schema script

--- schema.sql
+++ schema.sql
@@ -1,25 +1,38 @@
-create database Scheduler;
+create database if not exists scheduler;
+grant all on scheduler.* to 'scheduler_user'@'localhost' identified by '$chedulerp@$$';
 
---create tables
+use scheduler;
 
-create table User(
-    uID int NOT NULL AUTO_INCREMENT,
-    lastName varchar(255),
-    firstName varchar(255),
-    username varchar(255),
-    emailAddress varchar(255),
-    PRIMARY KEY (uID)
+create table if not exists `user` (
+    user_id int(11) NOT NULL AUTO_INCREMENT,
+    lastname varchar(100),
+    firstname varchar(100),
+    username varchar(100) UNIQUE NOT NULL,
+    email_address varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (user_id)
 );
 
-create table Calendar(
-    calID int NOT NULL AUTO_INCREMENT,
-    calName
-    calColor
-    PRIMARY KEY (uID),
-    FOREIGN KEY (calID) REFERENCES User(uID)
+create table if not exists calendar (
+    cal_id int(11) NOT NULL AUTO_INCREMENT,
+    user_id int(11) NOT NULL,
+    name varchar(100) NOT NULL,
+    color varchar(10) NOT NULL,
+    date_created datetime NOT NULL,
+    PRIMARY KEY (cal_id),
+    FOREIGN KEY (user_id) REFERENCES `user` (user_id)
 );
 
-create table Event(
-    
+create table if not exists event (
+    event_id int(11) NOT NULL AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    is_all_day tinyint(1) NOT NULL,
+    date_start datetime NOT NULL,
+    date_end datetime NOT NULL,
+    recurrence_type varchar(100),
+    cal_id int(11) NOT NULL,
+    date_created datetime NOT NULL,
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (cal_id) REFERENCES calendar(cal_id)
 );
 
