18fa793cf9a57865c730cdab9cbb211f439ef17c
DRaine Edrosa <ltedrosa@gmail.com>
2015-05-19 11:16:24 +0800

    create event

--- application/controllers/Event.php
+++ application/controllers/Event.php
@@ -6,52 +6,81 @@ class Event extends CI_Controller {
     }
 
     public function index(){
-   }
+    }
 
     public function form(){
-      $this->load->model('event_model');
-      $data['main_content'] = 'event_form';
-      $this->load->view('includes/template', $data);
-   }
+        $this->load->model('calendar_model');
+        $calendars = $this->calendar_model->get_calendars($this->session->userdata('user_id'));
+        $data['calendars'] = array();
+        foreach ($calendars as $calendar) {
+            $data['calendars'][$calendar['cal_id']] = $calendar['name'];
+        }
 
-   public function new_event(){
-      $errors = array();
-      if($_SERVER['REQUEST_METHOD'] == 'POST'){
-         $this->form_validation->set_rules('name', 'Event Name', 'required');
-         $this->form_validation->set_rules('start_date', 'Start Date', 'required');
-         $this->form_validation->set_rules('end_date', 'End Date', 'required');
+        $data['main_content'] = 'event_form';
+        $this->load->view('includes/template', $data);
+    }
 
+    public function new_event(){
+        $errors = array();
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $this->form_validation->set_rules('name', 'Event Name', 'required');
+            $this->form_validation->set_rules('date_start', 'Start Date', 'required');
+            $this->form_validation->set_rules('date_end', 'End Date', 'required');
 
-         if($this->form_validation->run() == TRUE) {
-            $data = array(
-              'name'             => $this->session->userdata('name'),
-              'is_all_day'       => $this->input->post("is_all_day", TRUE),
-             // 'date_start'       => date($this->input->post("date_start", TRUE), $this->input->post("time_start", TRUE)); ,
-              //'date_end'         => $this->input->post("date_end", TRUE),
-              'recurrence_type'  => $this->input->post("recurrence_type", TRUE),
-             // 'cal_id'           => $this->input->post("cal_id", TRUE),
-              'date_created'             => date("Y-m-d H:i:s")
-            );
+            if($this->form_validation->run() == TRUE) {
+                $dates = $this->check_date($this->input->post('date_start'), $this->input->post('date_end'),
+                    $this->input->post('time_start'), $this->input->post('time_end'));
 
-            $this->load->model('event_model');
-            $result = $this->event_model->create($data);
+                if ($dates === FALSE) {
+                    $errors['date_start'] = "Start date must be less than End date.";
+                } else {
+                    $is_all_day = $this->input->post("is_all_day", TRUE) ? 1 : 0;
+                    $data = array(
+                        'name'            => $this->input->post("name", TRUE),
+                        'is_all_day'      => $is_all_day,
+                        'date_start'      => $dates['start'],
+                        'date_end'        => $dates['end'],
+                        'cal_id'          => $this->input->post("cal_id", TRUE),
+                        'recurrence_type' => $this->input->post("recurrence_type", TRUE),
+                        'date_created'    => date("Y-m-d H:i:s")
+                    );
 
-            if ($result['status'] == 'success') {
-                header("Location: " . site_url(array('calendar')));
-            } else {
-                $errors['warning'] = $result['error'];
+                    $this->load->model('event_model');
+                    $result = $this->event_model->create($data);
+
+                    if ($result['status'] == 'success') {
+                        $this->session->set_flashdata("succcess", "You have successfully created an event!");
+                        header("Location: " . site_url(array('calendar')));
+                    } else {
+                        $errors['warning'] = $result['error'];
+                    }
+                }
             }
+        }
+        $data = array(
+            'errors'       => $errors,
+            'main_content' => 'event_form.php'
+        );
 
-         } else {
-            $data = array(
-                'errors'       => $errors,
-                'main_content' => 'event_form.php'
-            );
-            $this->load->view("includes/template", $data);
-            return;
-         }
-      }
+        $this->load->model('calendar_model');
+        $calendars = $this->calendar_model->get_calendars($this->session->userdata('user_id'));
+        $data['calendars'] = array();
+        foreach ($calendars as $calendar) {
+            $data['calendars'][$calendar['cal_id']] = $calendar['name'];
+        }
+        
+        $this->load->view("includes/template", $data);
+        return;
+    }
 
-   }
+    public function check_date($start_date, $end_date, $start_time=NULL, $end_time=NULL) {
+        $start = $start_date . " " . ($start_time ? ($start_time . ":00") : "00:00:00");
+        $end = $end_date . " " . ($end_time ? ($end_time . ":00") : "00:00:00");
+
+        if (strtotime($end) < strtotime($start)) {
+            return FALSE;
+        }
+        return array("start" => $start, "end" => $end);
+    }
 }
 ?>
--- application/views/event_form.php
+++ application/views/event_form.php
@@ -7,6 +7,11 @@
                 echo heading('Create new event', 3);
                 echo validation_errors();
 
+                if (isset($errors['date_start'])) {
+                    echo $errors['date_start'];
+                    echo br(2);
+                }
+
                 echo form_open('event/new_event');
 
                 echo form_input(array(
@@ -14,50 +19,56 @@
                     'placeholder' => 'Event Name',
                     'value'       => set_value('name')));
 
-               echo form_label('Start Date', 'date_start');
-               echo nbs(3);
-               echo form_input(array(
+                echo form_label('Calendar', 'calendar');
+                echo nbs(2);
+                echo form_dropdown('cal_id', $calendars);
+                echo br(1);
+
+                echo form_label('All day event', 'is_all_day');
+                echo nbs(2);
+                echo form_checkbox(array(
+                    'name'      => 'is_all_day',
+                    'value'     => 1,
+                    'checked'   => FALSE));
+                echo br(1);
+
+                echo form_label('Start Date', 'date_start');
+                echo nbs(3);
+                echo form_input(array(
                      'name'   => 'date_start',
                      'type'   => 'date',
                      'value'  => set_value('date_start')));
-               echo form_input(array(
+                echo form_input(array(
                      'name'   => 'time_start',
                      'type'   => 'time',
                      'value'  => set_value('time_start')));
-               echo br(1);
+                echo br(1);
 
-               echo form_label('End Date', 'date_end');
-               echo nbs(4);
-               echo form_input(array(
+                echo form_label('End Date', 'date_end');
+                echo nbs(4);
+                echo form_input(array(
                      'name'   => 'date_end',
                      'type'   => 'date',
                      'value'  => set_value('date_end')));
-               echo form_input(array(
+                echo form_input(array(
                      'name'   => 'time_end',
                      'type'   => 'time',
                      'value'  => set_value('time_end')));
-               echo br(2);
-
-               echo form_label('All day event', 'is_all_day');
-               echo nbs(2);
-               echo form_checkbox(array(
-                  'name'      => 'is_all_day',
-                  'value'     => 1,
-                  'checked'   => FALSE));
-               echo nbs(5);
-               $recurrence = array(
-                  'never' => 'Never',
-                  'daily' => 'Daily',
-                  'weekly' => 'Weekly',
-                  'monthly' => 'Monthly',
-                  'yearly' => 'Yearly'
-               );
-               echo form_label('Repeat', 'recurrence');
-               echo nbs(2);
-               echo form_dropdown('recurrence_type', $recurrence, 'never');
-               echo br(3);
-               echo form_submit('submit', 'Create Event');
-               echo anchor('calendar', 'Cancel')
+                echo br(2);
+               
+                $recurrence = array(
+                    'never' => 'Never',
+                    'daily' => 'Daily',
+                    'weekly' => 'Weekly',
+                    'monthly' => 'Monthly',
+                    'yearly' => 'Yearly'
+                );
+                echo form_label('Repeat', 'recurrence');
+                echo nbs(2);
+                echo form_dropdown('recurrence_type', $recurrence, 'never');
+                echo br(3);
+                echo form_submit('submit', 'Create Event');
+                echo anchor('calendar', 'Cancel')
             ?>
 
         </div>
