7a5103ee4ba646f8e496ad1146acdbcae45f33fe
Cai <cai.abbass@gmail.com>
2015-05-25 04:32:43 +0800

    styling for login page

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -7,7 +7,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
    <title>Welcome to Skedjul</title>
 
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css" type="text/css" media="screen" charset="utf-8">
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap-theme.css" type="text/css" media="screen" charset="utf-8">
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css" type="text/css" media="screen" charset="utf-8">
 </head>
    <body>
--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,23 +1,41 @@
 <div id="container">
-    <h1>Welcome to Skedjul!</h1>
+   <div class="container">
+      <div class="jumbotron" style="margin-top: 40px">
+         <h1>Welcome to Skedjul!</h1>
+         <p>Login or signup for an account to get started.</p>
+      </div>
+
 
 	<div id="body">
-		<p>Login or signup for an account to get started.</p>
 
-        <div id="login_form">
+
+        <div class="form-control">
         <?php
             echo heading('Login', 3);
             echo validation_errors();
             if (isset($errors['warning'])) {
                 echo $errors['warning'];
             }
+            $attribute = array('class' => 'form-control');
             echo form_open('login');
             echo form_input(array('name' =>'username','placeholder' => 'Username'));
             echo form_password(array('name' =>'password','placeholder' => 'Password'));
-            echo form_submit('submit', 'Login');
-            echo anchor('signup', 'Signup for an account');
+
+            $attributes = array(
+               'class' => 'btn btn-primary',
+               'role'  => 'button'
+            );
+            echo form_submit('submit', 'Login', "class='btn btn-primary'");
+
+            $attributes = array(
+               'class' => 'btn btn-default',
+               'role'  => 'button'
+            );
+            echo anchor('signup', 'Signup for an account', $attributes);
         ?>
         </div>
+     </div>
         <?php echo br(2);?>
 	</div>
+</div>
 </div>
