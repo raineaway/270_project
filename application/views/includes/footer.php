ed832650da8a677ba4a2b513943aabda554d1c75
Cai <cai.abbass@gmail.com>
2015-05-25 18:03:02 +0800

    fixed site url in events, updated time display in dashboard

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,3 +1,5 @@
-            <em>&copy; 2015</em>
+            <div style="width:100%; margin:auto; color: gray">
+            <p class="text-right"><em>Raineaway & Pinkurinal &copy; 2015</em></p>
+            </div>
         </body>
 </html>
